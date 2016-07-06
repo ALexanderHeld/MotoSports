192e2ca3c263f49c56c0e9495c9323d647d2702b
AlexanderHeld <alexanderheld43@gmail.com>
2016-07-06 22:21:11 +0200

    Update 1.0

---
+++ MotoSports/Fahrer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoSports
+{
+    class Fahrer
+    {
+        // Alter, Name etc
+        private string Name { get; set; }
+        private int Alter { get; set; }
+        private int Gewicht { get; set; }
+
+        public void SetName(string name)
+        {
+            if (!String.IsNullOrEmpty(name))
+                Name = name;
+            else
+                // Fehlerbearbeitung
+                throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            if (!String.IsNullOrEmpty(Name))
+                return Name;
+            else
+                throw new NotImplementedException();
+                return null;
+        }
+    }
+}
